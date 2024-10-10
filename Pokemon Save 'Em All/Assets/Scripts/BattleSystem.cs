c5588afc4ab037a44755331809b3d2fa4f153a4c
Maxence <96044322+mxnfrm14@users.noreply.github.com>
2024-10-10 11:48:48 +0200

    start game

--- Pokemon Save 'Em All/Assets/Scripts/BattleSystem.cs
+++ Pokemon Save 'Em All/Assets/Scripts/BattleSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 public class BattleSystem : MonoBehaviour
@@ -12,18 +13,28 @@ public class BattleSystem : MonoBehaviour
     public Transform playerBattleStation;
     public Transform enemyBattleStation;
 
+    public Text dialogueText;
+    Unit playerUnit;
+    Unit enemyUnit;
+
     // Start is called before the first frame update
     void Start()
     {
         state = BattleState.START;
+        SetupBattle();
     }
 
 
     void SetupBattle()
     {
-        Instantiate(playerPrefab, playerBattleStation);
-        Instantiate(enemyPrefab, enemyBattleStation);
-        state = BattleState.PLAYERTURN;
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+
+        dialogueText.text= enemyUnit.unitName + " has appeared!";
+        //state = BattleState.PLAYERTURN;
     }
 
 }
